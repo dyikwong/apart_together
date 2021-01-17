9257ee09afbe82e11eeead5505baff5c67b219da
anthony-huynh <anthuynh@ucdavis.edu>
2021-01-16 21:09:58 -0800

    calendar code
    the ic_close_black xml file goes in the Drawable directory under /res

---
+++ MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import androidx.annotation.NonNull; //i had to use this instead of import android.support.annotation.NonNull;
+import android.widget.PopupWindow;
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.view.ViewGroup.LayoutParams;
+
+
+public class MainActivity extends AppCompatActivity {
+    CalendarView Calendar;
+    TextView date_view;
+
+    private Context mContext;
+    private Activity mActivity;
+
+    private RelativeLayout mRelativeLayout;
+    private Button mButton;
+
+    private PopupWindow mPopupWindow;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = getApplicationContext();
+        mActivity = MainActivity.this;
+
+        // Get the widgets reference from XML layout
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.rl);
+        mButton = (Button) findViewById(R.id.viewEvent);
+
+        Calendar = (CalendarView) findViewById(R.id.Calendar);
+        date_view = (TextView) findViewById(R.id.date_view);
+
+        Calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                            @Override
+                            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                                String Date = (month + 1) + "-" + dayOfMonth + "-" + year;
+                                // set this date in TextView for Display
+                                date_view.setText(Date);
+                            }
+
+                        });
+
+
+        // Set a click listener for the text view
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public void onClick(View view) {
+                // Initialize a new instance of LayoutInflater service
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                // Inflate the custom layout/view
+                View customView = inflater.inflate(R.layout.custom_layout,null);
+
+                mPopupWindow = new PopupWindow(
+                        customView,
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT
+                );
+
+                if(Build.VERSION.SDK_INT>=21){
+                    mPopupWindow.setElevation(5.0f);
+                }
+                ImageButton closeButton = (ImageButton) customView.findViewById(R.id.ib_close);
+
+
+                //ImageButton closeButton = (ImageButton) customView.findViewById(R.id.ib_close);
+
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mPopupWindow.dismiss();
+                    }
+                });
+                mPopupWindow.showAtLocation(mRelativeLayout, Gravity.CENTER,0,0);
+            }
+        });
+    }
+
+}
